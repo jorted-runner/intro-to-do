8bc4909b531c4efbf9b6887936e73a95e99161e4
jorted-runner <jorted.python@gmail.com>
2025-05-07 16:48:01 -0500

    Toggle todo and delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,24 @@ export default function App() {
         },
       ]
     })
+      setNewItem('');
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -32,14 +50,20 @@ export default function App() {
 			<ul className="list">
 				{todos.map(todo => {
           return (
-            <li key={todo.id}>
-              <label>
-                <input type="checkbox" checked={todo.completed} />
-                {todo.title}
-              </label>
-              <button className="btn btn-danger">Delete</button>
-            </li>
-          )
+						<li key={todo.id}>
+							<label>
+								<input
+									type="checkbox"
+									checked={todo.completed}
+									onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+								/>
+								{todo.title}
+							</label>
+							<button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">
+								Delete
+							</button>
+						</li>
+					);
         })}
 			</ul>
 		</>
